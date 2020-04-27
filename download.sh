ba1ab082f7aeb714e06a480f4cf05898b83edefc
Shoab Saadat <dr.shoaibsaadat@gmail.com>
2020-04-27 18:43:41 +0100

    first commit with new download link

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget https://de.cyverse.org/dl/d/6FE1A5C4-4140-420E-8F32-4EC6250E16D5/vina_prep.zip
+wget https://de.cyverse.org/dl/d/53940A36-5E17-40DE-9E81-254B3216A25C/vina_prep.zip
 unzip vina_prep.zip
 mv ./vina_prep ./Vina_Project
 rm vina_prep.zip 
